acd3d61de570527351da49c939791e0d75f0f30f
AhmedGeek <AhmedGeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-24 03:26:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facebook-like-button/tags/5.1@280005 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/live_fill.php
@@ -0,0 +1,9 @@
+<?php
+
+/*Live Stream auto fill system*/
+
+$app_id_value = (get_option('fpp_live_appid') != '')  ? get_option('fpp_live_appid') : '';
+$width_value  = (get_option('fpp_live_width') != '')  ? get_option('fpp_live_width') : '';
+$height_value = (get_option('fpp_live_height') != '') ? get_option('fpp_live_height') : '';
+$wid_value    = (get_option('fpp_live_wid_title') != '') ? get_option('fpp_live_wid_title') : '';
+?>
